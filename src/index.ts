57f2512ed80600d889df224f3eb17cdec3f6c538
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-12-13 08:55:59 -0300

    remove multer middleware and image routes from index

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,16 @@
 import { logger } from './logger';
 import { checkPublicDir } from './util';
 import express from 'express';
-import multer from 'multer';
-import sharp from 'sharp';
+import imageRouter from './routers/image';
 
 const S3_PORT = process.env.PORT || 4001;
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
 
 checkPublicDir();
 
 const app = express();
+
 app.use(express.static('./public'));
-
-app.post('/upload/image', upload.single('file'), async (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ err: 'No file sent' });
-    }
-
-    const { buffer, originalname } = req.file;
-    const timestamp = new Date().toISOString();
-    const ref = `${timestamp}-${originalname.split('.')[0]}.webp`;
-    await sharp(buffer)
-        .webp({ quality: 40 })
-        .toFile('./public/images/' + ref);
-    const imgPath = `/images/${ref}`;
-
-    return res.status(200).json({ imgPath });
-})
+app.use(imageRouter.router);
 
 app.listen(S3_PORT, () => {
     logger.log(`Server listening on port ${S3_PORT}`)
