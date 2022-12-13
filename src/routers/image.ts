e3bae3a4bee419edd59f3760f86fcd063177a3c3
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-12-13 09:27:40 -0300

    image options validator

--- src/routers/image.ts
+++ src/routers/image.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
 import sharp from 'sharp';
 import { upload } from "../util";
+import { randomUUID } from 'crypto';
 
 const router = Router();
 
 type ImageUploadOptions = {
     quality?: number
+}
+
+function validateOptions(options: ImageUploadOptions) {
+    if (options.quality) {
+        if (options.quality > 100 || options.quality < 10) {
+            return { status: 400, err: 'Quality needs to be in a range between 100 an 10.' }
+        }
+    }
+
+    return null
 }
 
 router.post('/upload/image', upload.single('file'), async (req, res) => {
@@ -14,6 +25,10 @@ router.post('/upload/image', upload.single('file'), async (req, res) => {
     }
 
     const options: ImageUploadOptions = req.body;
+
+    const error = validateOptions(options);
+
+    if (error) return res.status(error.status).json({ err: error.err });
 
     const { buffer, originalname } = req.file;
     const timestamp = new Date().toISOString();
